49d40ae9ffb9d50189f81e73060346aaf6260cb6
Grillot Clément <c.c.grillot@gmail.com>
2013-04-11 09:08:11 +0200

    Kebab
    KEbab everywhere

--- bin/trilog.sh
+++ bin/trilog.sh
@@ -1,67 +1,72 @@
-#!/bin/bash
+# !bin/bash/
 
-#  trilog.sh
-#
-#  Effectue des actions sur des logs selon l’option utilisée.
-#  Permet par exemple un affichage en html ou des sauvegardes.
-#
-#  Created by Raphaël Gressier on 04/04/13.
-
-VERBOSE=0
-ARCHIVE=0
-WEB=0
-CHEMIN
-ERROR=0
-WARNING=0
-
-while (( $# ))
+#Detection des options
+while [ $# -ge 1 ]
 do
-    case $1 in
-
-    "-h" | "--help")
-        echo "####################################################################"
-        echo "Bienvenue dans l'aide de Trilog"
-        echo "####################################################################"
-        more HELP.txt
-        echo -e "\n"
-        exit 0
-        ;;
-    "-v" | "-verbose")
-        V=1 #Si l'utilisateur demande le mode verbose.
-        shift
-        ;;
-    "-a | "--archive")
-        shift
-        ARCHIVE=1
-        CHEMIN=$1
-        shift
-        ;;
-    "-c | "--web")
-        shift
-        WEB=1
-        CHEMIN=$1
-        shift
-        ;;
-    "-e" | "--error")
-        shift
-        ERROR=1
-        shift
-        ;;
-    "-w" | "--warning")
-        shift
-        WARNING=1
-        shift
-        ;;
-    *)
-    if [[ -d $1 ]]
-        then
-        LISTE="$LISTE $1" # Concaténation des caractères de recherche.
-        shift
-        else
-        echo "Erreur : $1 argument inconnu!"
-        exit 6
-        fi
-    esac
+	
+	case $1 in
+	-h | --help)
+		aide=1 #Detection de la demande d'affichage de l'aide en ligne
+		;;
+	-v | --verbose)
+		description=1 #Detection de la demande de description des actions
+		;;
+	-a | --archive)
+		archivage=1 #Detection de la demande d'archivage des logs du jours
+		;;
+	-c | --web)
+		web=1 #Detection de la demande de cr�ation de l'arbo WEB
+		;;
+	-e | --error) #Detection de la d'affichage des erreur
+		error=1
+		;;
+	-w | --warning) # Detection de la demande d'affichage des alertes
+		warning=1
+		;;
+	esac
+	if [[ -d $1 ]] || [[ -f $1 ]]
+		then Cible="$1" #Detecte si la cible est un fichier ou un dossier
+	fi
+shift
 done
 
 
+#Execution des commmandes
+if [[ $aide -eq 1 ]]
+	then
+	if [[ $description -eq 1 ]]
+			then echo "Affichage de l'aide en ligne"
+	fi
+	cat ~/S1D/ASR3/NanoProjet/Projet/bin/HELP.txt
+	exit 0
+fi
+if [[ $error -eq 1 ]]
+	then 
+		if [[ $description -eq 1 ]]
+			then echo "Affichage des erreurs du jours"
+		fi
+		grep -r ERROR $Cible
+fi
+if [[ $warning -eq 1 ]]
+	then 
+		if [[ $description -eq 1 ]]
+			then echo "Affichage des alertes du jours"
+		fi
+		grep -r WARNING $Cible
+fi
+if [[ $archivage -eq 1 ]]
+	then
+		datejours=date +%d_%m_%y
+		if [[ $description -eq 1 ]]
+			then echo "Archivage des logs du $datejours en cours"
+		fi
+		cd ~/Projet/archive
+		tar zcvf $(date +%d_%m_%y).tar.gz $Cible
+fi
+if [[ $web -eq 1 ]]
+	then
+		if [[ $descritpion -eq 1 ]]
+			then echo "Cr�ation de l'arborescence web de $Cible"
+		fi
+		cp ~/Projet/web
+fi
